d1bd76c99d7b366d942fb205cfa2f42ae2eb4b66
Alessandro Rezzi <alessandrorezzi2000@gmail.com>
2023-11-19 18:43:08 +0100

    Add file with contants

---
+++ constants.h
@@ -0,0 +1,10 @@
+#ifndef constants
+#define constants
+
+// Useful physics constants
+constexpr double hslash = 1.0;
+
+// Define the small value used to compare float values
+constexpr double epsilon = 0.0001;
+
+#endif //constants
